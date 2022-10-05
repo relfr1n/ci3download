0228d8d84fed1f798dc18a9fcb0dd30e71a02249
relfr1n <renataelfrinwau@gmail.com>
2022-10-05 13:48:30 +0700

    commit pertama

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Renata Elfrin
+ Saya tingga di Kembangan Utara
+ saya kuliah di BSI";
+ }
+}
---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Renata</li>
+        <li>Nama Belakang : Elfrin</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Puri Kembangan Jakarta Barat</li>
+    </ul>
+ 
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Sumut</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Tenis Meja</li>
+        <li>Catur</li>
+    </ul>
+</section>
